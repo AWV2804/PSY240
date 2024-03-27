362907cb84aaf1e75539779346f5119a57a14464
Atharva Rao <atharvarao100@gmail.com>
2024-03-27 18:39:39 -0400

    Initial Commit

---
+++ PSY240/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PSY240
+//
+//  Created by Atharva Rao on 3/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PSY240/PSY240App.swift
@@ -0,0 +1,17 @@
+//
+//  PSY240App.swift
+//  PSY240
+//
+//  Created by Atharva Rao on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct PSY240App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
